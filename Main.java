692e7b63565486a22fc742aaaafcbec2f23569fa
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-28 23:59:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
+//package salarycalc
 
 public class Main {
-	public static void main(String[] args){
-		
-		Employee e1 = new Employee("Gwen", 2000, 45, 1985);
-		e1.printInfo();
-		
-	}
+    public static void main(String[] args) {
+        
+        Employee emp1 = new Employee("Carlynne Clackson", 16000, 45, 1999);
+        emp1.printInfo();
+        
+        Employee emp2 = new Employee("Rick Geratt", 5000, 30, 2005);
+        emp2.printInfo();
+        
+        Employee emp3 = new Employee("Grata Tomas", 1500, 2, 2020);
+        emp3.printInfo();
+        
+        Employee emp4 = new Employee("Maurice Oldford", 7500, 41, 2010);
+        emp4.printInfo();
 
+
+    }
 }
